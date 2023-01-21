e8e153261fdd27e20d007026897120d1304572a8
Kleber Nuñez <kgnunez@espol.edu.ec>
2023-01-20 20:40:40 -0500

    Actualizacion

--- models/routes.js
+++ models/routes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/connection_ssh', {
+mongoose.connect('mongodb+srv://kgnunez:SKnnyIIB6dpLvqnG@cluster0.kesacsi.mongodb.net/test', {
     useNewUrlParser: true,
 },
 (err) => {
