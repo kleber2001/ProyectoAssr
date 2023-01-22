b28c89c802771281687a3fe0a4aad166a4437b80
Kleber Nuñez <kgnunez@espol.edu.ec>
2023-01-22 17:20:50 -0500

    conexión a base de datos

--- models/routes.js
+++ models/routes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true)
-mongoose.connect('mongodb+srv://kgnunez:SKnnyIIB6dpLvqnG@cluster0.kesacsi.mongodb.net/test', {
+mongoose.connect('mongodb+srv://kgnunez:SKnnyIIB6dpLvqnG@cluster0.kesacsi.mongodb.net/connection_ssh', {
     useNewUrlParser: true,
 },
 (err) => {
